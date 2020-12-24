1e97436ca2ca9b7ebeff37ac748861782b3fbfa1
Sergei <sergeitimofeev76@gmail.com>
2020-12-24 21:48:17 +0300

    первый коммит

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sergei/Neuronet_Project/Simple_Neuronet
+# Build directory: /home/sergei/Neuronet_Project/Simple_Neuronet/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
